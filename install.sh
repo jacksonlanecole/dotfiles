8431181872c3d01688d8d7a86a1e351772ddb645
Jackson Cole <jackson@jacksoncole.io>
2019-04-21 17:49:14 -0500

    Updated install script

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin maste
 
 ln -svf "$DOTFILES_DIR/tmux.conf" $HOME/.tmux.conf
 ln -svf "$DOTFILES_DIR/vimrc/vimrc-noplugins" $HOME/.vimrc
+ln -svf "$DOTFILES_DIR/chunkwmrc" $HOME/.chunkwmrc
+ln -svf "$DOTFILES_DIR/skhdrc" $HOME/.skhdrc
