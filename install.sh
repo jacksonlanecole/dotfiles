f030bc737035504811d43e3e2b0ad3c9810a7fbe
Jackson L. Cole <jackson.lane.cole@gmail.com>
2019-04-21 10:53:46 -0500

    Removed conditional. Now The repo is updated every time the script is run.

--- install.sh
+++ install.sh
@@ -3,9 +3,7 @@
 export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [[ -x 'git -a -d "$DOTFILES_DIR/.git"' ]]
-  then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
-fi
+git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 ln -svf "$DOTFILES_DIR/tmux.conf" $HOME/.tmux.conf
 ln -svf "$DOTFILES_DIR/vimrc/vimrc-noplugins" $HOME/.vimrc
