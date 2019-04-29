52d0ca78ba6b40bcdd2a8128ce9e3078fbc837c2
Jackson Cole <jackson@jacksoncole.io>
2019-04-29 07:13:37 -0500

    Added conditional to only make symlinks for macOS specific dotfiles on darwin based systems

--- install.sh
+++ install.sh
@@ -7,5 +7,8 @@ git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin maste
 
 ln -svf "$DOTFILES_DIR/tmux.conf" $HOME/.tmux.conf
 ln -svf "$DOTFILES_DIR/vimrc/vimrc-noplugins" $HOME/.vimrc
-ln -svf "$DOTFILES_DIR/chunkwmrc" $HOME/.chunkwmrc
-ln -svf "$DOTFILES_DIR/skhdrc" $HOME/.skhdrc
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	ln -svf "$DOTFILES_DIR/chunkwmrc" $HOME/.chunkwmrc
+	ln -svf "$DOTFILES_DIR/skhdrc" $HOME/.skhdrc
+fi
