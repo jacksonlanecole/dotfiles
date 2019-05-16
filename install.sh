27850a7fda7008b41839b94726cafeecd17a874a
Jackson Cole <jackson.cole@apexturbine.com>
2019-05-16 16:03:11 -0500

    Update install script to install the minimal plugin vimrc

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 ln -svf "$DOTFILES_DIR/tmux.conf" $HOME/.tmux.conf
-ln -svf "$DOTFILES_DIR/vimrc/vimrc-noplugins" $HOME/.vimrc
+ln -svf "$DOTFILES_DIR/vimrc/vimrc-minimal" $HOME/.vimrc
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	ln -svf "$DOTFILES_DIR/chunkwmrc" $HOME/.chunkwmrc
