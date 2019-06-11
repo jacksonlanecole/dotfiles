2c37ea96fc7bfcc1e9a7841a214267149cd5c013
Jackson Cole <jackson@jacksoncole.io>
2019-06-10 20:29:22 -0500

    Change hashbang from /usr/bin/env bash -> /bin/sh, remove macos support
    Jackson Cole <jackson@jacksoncole.io>

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -8,8 +8,3 @@ git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin maste
 ln -svf "$DOTFILES_DIR/tmux.conf" $HOME/.tmux.conf
 ln -svf "$DOTFILES_DIR/vimrc/vimrc-minimal" $HOME/.vimrc
 ln -svf "$DOTFILES_DIR/.profile" $HOME/.profile
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	ln -svf "$DOTFILES_DIR/chunkwmrc" $HOME/.chunkwmrc
-	ln -svf "$DOTFILES_DIR/skhdrc" $HOME/.skhdrc
-fi
