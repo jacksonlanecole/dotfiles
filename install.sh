d92dda66a9571358169ebb76807da8b38177eb05
Jackson Cole <jackson@jacksoncole.io>
2019-06-27 00:47:15 -0500

    Reflect rename of .profile -> profile in dotfiles dir in install.sh
    Jackson Cole <jackson@jacksoncole.io>

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ ln -svf "$dotfiles_dir/tmux.conf" $HOME/.tmux.conf
 ln -svf "$dotfiles_dir/vimrc/vimrc-minimal" $HOME/.vimrc
 ln -svf "$dotfiles_dir/inputrc" $HOME/.inputrc
 ln -svf "$dotfiles_dir/bashrc" $HOME/.bashrc
-ln -svf "$dotfiles_dir/.profile" $HOME/.profile
+ln -svf "$dotfiles_dir/profile" $HOME/.profile
