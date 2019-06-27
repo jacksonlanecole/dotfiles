a7d4edefc4763074d642aec217124f911da6284a
Jackson Cole <jackson@jacksoncole.io>
2019-06-27 00:45:03 -0500

    Add inputrc to install.sh
    Jackson Cole <jackson@jacksoncole.io>

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ git --work-tree="$dotfiles_dir" --git-dir="$dotfiles_dir/.git" pull origin maste
 
 ln -svf "$dotfiles_dir/tmux.conf" $HOME/.tmux.conf
 ln -svf "$dotfiles_dir/vimrc/vimrc-minimal" $HOME/.vimrc
+ln -svf "$dotfiles_dir/inputrc" $HOME/.inputrc
 ln -svf "$dotfiles_dir/bashrc" $HOME/.bashrc
 ln -svf "$dotfiles_dir/.profile" $HOME/.profile
